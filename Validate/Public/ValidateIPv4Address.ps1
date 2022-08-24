c9fd42a890f2fefa4cdc219937474193695ca3f5
Josh Fogus <josh.fogus@gmail.com>
2022-08-24 13:05:04 -0700

    Changed parameter casing for uniformity.

--- Validate/Public/ValidateIPv4Address.ps1
+++ Validate/Public/ValidateIPv4Address.ps1
@@ -1,8 +1,8 @@
 function ValidateIPv4Address {
     param(
-        [String][Parameter(Position=0)] $ipAddress
+        [String][Parameter(Position=0)] $IPAddress
     )
 
     $regex = "^(((2(?=[0-5]{2})[0-5]{2})|(1(?=[0-9]{2})[0-9]{2})|([0-9](?<![0-9]{2})(?![0-9]{2})[1-9]?)).){3}((2(?=[0-5]{2})[0-5]{2})|(1(?=[0-9]{2})[0-9]{2})|([0-9](?<![0-9]{2})(?![0-9]{2})[1-9]?))$"
-    return $ipAddress -match $regex
+    return $IPAddress -match $regex
 }
